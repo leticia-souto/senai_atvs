7ad690ccca67236dd105903e8d5cf7ff924498f6
leticia-souto <leticia.souto@aluno.senai.br>
2026-02-26 15:54:25 -0300

    exercicio front 26/02

---
+++ Senai semestre3/frontEnd/aula4_javaScript/exercicio/script/script.js
@@ -0,0 +1,52 @@
+//Exercicio 1
+
+// const usuario = prompt('Seja Bem-Vindo, cliente! Para um atendimento personalizado, digite seu nome:')
+
+// const usuarioSobrenome = prompt(`${usuario}, agora digite seu sobrenome:`)
+
+// let nomeFormatado = (usuario.concat(usuarioSobrenome)).toLowerCase()
+
+// alert(`Olá, ${nomeFormatado}!!`)
+// alert(`${nomeFormatado}, seu nome possui ${nomeFormatado.length} caracteres.`)
+
+
+//Exercicio 2
+
+// const conta = prompt('Seja Bem-Vindo, usuário! Irei te ajudar a dividir a conta, me informe o valor total:')
+
+// const qtdPessoas = prompt('Certo! Agora me informe a quantidade de pessoas da mesa:')
+
+// let valorPorPessoa = (conta/qtdPessoas).toFixed(2)
+
+// alert(`O valor que cada pessoa deve pagar é de R$${valorPorPessoa}`)
+
+
+//Exercicio 3
+
+// const valorCompra = prompt ('Olá cliente, iriei te informar se seu pedido entra na promoção. Me informe o valor da compra:')
+// const cupom = confirm ('Você possui cupom?')
+
+// if (valorCompra > 150 || cupom == true){
+//     console.log(`Frete Liberado!!`)
+// } else{
+//     console.log(`Frete Pago :(`)
+// }
+
+
+//Exercicio 4
+
+// const numeroUsuario = prompt ('Olá usuário! Vamos fazer um sorteio, escolha um número de 1 a 10.')
+
+// let numeroSistema = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+
+// if (numeroUsuario >10 && numeroUsuario <=0){
+//     alert(`Número Inválido!!`)
+// }
+// else if(numeroUsuario == numeroSistema ){
+//     alert(`Parabéns!! Você ganhou o sorteio número sorteado foi ${numeroSistema}`)
+    
+// } 
+// else{
+//     alert(`Infelizmente o número sorteado foi ${numeroSistema}.`)
+// }
+
