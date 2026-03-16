37a6d11d9c911363be17781fcac9fe4e8eb0a85f
leticia-souto <leticia.souto@aluno.senai.br>
2026-03-16 15:58:25 -0300

    alteracao

--- Senai semestre3/mobile/projeto/react_native/first-app/src/jsx_formativa/exercicio10.js
+++ Senai semestre3/mobile/projeto/react_native/first-app/src/jsx_formativa/exercicio10.js
@@ -4,35 +4,35 @@ export default function ExercicioView10() {
     return <View style={styles.container}>
 
         <Text>Mini Dashboard</Text>
-    
+
         <View style={styles.container}>
 
             <View style={styles.header}>
                 <Text style={styles.textStyle}>Header</Text>
             </View>
 
-            <View style={styles.row}>
-                    <View style={styles.redBox}>
-                        <Text style={styles.textStyle}>1</Text>
-                    </View>
+            <View style={styles.rowTop}>
+                <View style={styles.redBox}>
+                    <Text style={styles.textStyle}>1</Text>
+                </View>
 
-                    <View style={styles.blueBox}>
-                        <Text style={styles.textStyle}>2</Text>
-                    </View>
+                <View style={styles.blueBox}>
+                    <Text style={styles.textStyle}>2</Text>
+                </View>
 
-                    <View style={styles.yellowBox}>
-                        <Text style={styles.textStyle}>3</Text>
-                    </View>
+                <View style={styles.yellowBox}>
+                    <Text style={styles.textStyle}>3</Text>
+                </View>
             </View>
 
-            <View style={styles.row}>
-                    <View style={styles.painelPrincipal}>
-                        <Text style={styles.textStyle}>Painel Principal</Text>
-                    </View>
+            <View style={styles.rowMain}>
+                <View style={styles.painelPrincipal}>
+                    <Text style={styles.textStyle}>Painel Principal</Text>
+                </View>
 
-                    <View style={styles.lateral}>
-                        <Text style={styles.textStyle}>Lateral</Text>
-                    </View>
+                <View style={styles.lateral}>
+                    <Text style={styles.textStyle}>Lateral</Text>
+                </View>
 
             </View>
 
@@ -113,10 +113,15 @@ const styles = StyleSheet.create({
         gap: 8,
 
     },
-    row: {
+    rowTop: {
+        flexDirection: "row",
+        height: 40,
+        gap: 8,
+    },
+
+    rowMain: {
         flexDirection: "row",
         flex: 1,
         gap: 8,
-
-    }
+    },
 });
