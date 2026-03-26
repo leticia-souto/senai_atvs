c2ee4e1455718fa5fc0e7acd65c6690177d46dd1
leticia-souto <leticia.souto@aluno.senai.br>
2026-03-26 14:21:29 -0300

    formativa front atualização 26/03

--- Senai semestre3/frontEnd/formativa_DOM_aula6/src/scripts/script-aula06.js
+++ Senai semestre3/frontEnd/formativa_DOM_aula6/src/scripts/script-aula06.js
@@ -11,23 +11,21 @@ inputcurtir.addEventListener("click", () => {
 const input = document.querySelector("#campo-texto");
 const preview = document.querySelector("#preview-texto");
 
-if (input && preview) {
-    input.addEventListener("input", () => {
-        preview.textContent = "Digitando: " + input.value;
-    });
-}
+
+input.addEventListener("input", () => {
+    preview.textContent = "Digitando: " + input.value;
+});
+
 
 const caixa = document.querySelector("#caixa-cor");
 
 if (caixa) {
 
-    // quando o mouse entra
     caixa.addEventListener("mouseenter", () => {
         caixa.style.backgroundColor = "blue";
     });
 
-    // quando o mouse sai
     caixa.addEventListener("mouseleave", () => {
-        caixa.style.backgroundColor = ""; // volta ao original do CSS
+        caixa.style.backgroundColor = "";
     });
 }
