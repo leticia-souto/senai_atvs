1a5243838621905d407527ee9440a27b6dc93d71
leticia-souto <leticia.souto@aluno.senai.br>
2026-08-24 07:52:35 -0300

    aula 20/08 e 24/08

---
+++ SENAI semestre4/back/sabor_digital-main-celso/src/repositories/UsuarioRepository.js
@@ -0,0 +1,23 @@
+const pool = require('../config/database')
+
+class UsuarioRepository {
+    async create(usuarioData) {
+        const { nome, email, senha, papel } =
+            usuarioData,
+        const [result] = await pool.query(
+            'INSERT INTO usuario (nome, email, senha, papel) VALUES (?, ?, ?, ?)',
+            [nome, email, senha, papel || 'cliente']
+        );
+        return result.insertId;
+    }
+
+    async findByEmail(email){
+        const[rows] = await pool.query('SELECT * FROM usuario WHERE email = ?', [email]);
+        return rows[0];
+    }
+
+    async findById(id){
+        const[rows] = await pool.query('SELECT id, nome, papel, criado_em FROM usuario WHERE id = ?', [id]);
+        return rows[0];
+    }
+}
